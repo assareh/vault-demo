316c9821ea7dcf0442f618d225ea44736b65092c
assareh <assareh@hashicorp.com>
2020-04-16 21:08:09 -0700

    updating vault config script for containerized vault

--- files/configure_vault.sh
+++ files/configure_vault.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Script to configure Vault
 
-vault server -dev -dev-root-token-id=root &
-export VAULT_ADDR=http://localhost:8200
+ADDRESS=$(ifconfig eth0 | grep -E -o "(25[0-4]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-4]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-4]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-4]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | head -n 1)
+
+export VAULT_ADDR=http://$ADDRESS:8200
 export VAULT_TOKEN=root
 
 # Enable Auditing
@@ -14,7 +15,7 @@ vault secrets enable -path=lob_a/workshop/database database
 # Configure our secret engine
 vault write lob_a/workshop/database/config/ws-mysql-database \
     plugin_name=mysql-database-plugin \
-    connection_url="{{username}}:{{password}}@tcp(127.0.0.1:3306)/" \
+    connection_url="{{username}}:{{password}}@tcp($ADDRESS:3306)/" \
     allowed_roles="workshop-app" \
     username="root" \
     password="root"
